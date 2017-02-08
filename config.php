1d2ec82624c190e8419829ed9c1468ee3765763e
Nick Kramer <krames12@gmail.com>
2017-02-08 14:56:37 -0500

    some stuff was changed... just updating repo

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+return [
+  'database' => [
+    'name' => 'kramer-todo',
+    'username' => 'root',
+    'password' => '',
+    'connection' => 'mysql:host=localhost',
+    'options' => []
+  ]
+];
--- database/Connection.php
+++ database/Connection.php
@@ -1,10 +1,15 @@
 <?php
 
 class Connection {
-  public static function make() {
+  public static function make($config) {
     // of course for local and learning only -username and password wise-
     try {
-        return new PDO('mysql:host=localhost;dbname=kramer-todo', 'root', '');
+        return new PDO(
+          $config['connection'].';dbname='.$config['name'],
+          $config['username'],
+          $config['password'],
+          $config['options']
+        );
     } catch (PDOException $e) {
         die('Could not connect.');
     }
