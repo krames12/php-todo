171008ea5b69e5c65f01dacbc3a8d37c8a721e0b
Nick Kramer <krames12@gmail.com>
2017-02-09 01:21:22 +0000

    restructured files, added views and router.

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-$query = require "bootstrap.php";
+$query = require "core/bootstrap.php";
 
-$tasks = $query->selectAll('todos', 'Task');
+$router = new Router;
 
-require "index.view.php";
+require 'routes.php';
 
-?>
+require $router->direct();
