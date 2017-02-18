d06fa8085e23c4b4ac1b0e3e67ddc4e1f2120560
Nick Kramer <krames12@gmail.com>
2017-02-18 15:01:47 -0500

    handled users in seperate controller and view, cleaned up pages handling too

--- controllers/PagesController.php
+++ controllers/PagesController.php
@@ -2,15 +2,14 @@
 
 class PagesController {
   public function home() {
-    $users = App::get('database')->selectAll('users');
-    require "views/index.view.php";
+    return view('index');
   }
 
   public function about() {
-    require 'views/about.view.php';
+    return view('about');
   }
 
   public function contact() {
-    require 'views/contact.view.php';
+    return view('contact');
   }  
 }
--- routes.php
+++ routes.php
@@ -5,3 +5,6 @@
 $router->get('', 'PagesController@home');
 $router->get('about', 'PagesController@about');
 $router->get('contact', 'PagesController@contact');
+
+$router->get('users', 'UsersController@index');
+$router->post('users', 'UsersController@store');
